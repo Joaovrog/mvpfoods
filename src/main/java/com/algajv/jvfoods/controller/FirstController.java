cf9186a9454cac27ce259b1341a87525837ffcad
xb216442 <p000joao.vitor.vieira@prservicos.com.br>
2020-01-24 15:21:49 -0300

    Inicio codificacao | praticando

---
+++ src/main/java/com/algajv/jvfoods/controller/FirstController.java
@@ -0,0 +1,15 @@
+package com.algajv.jvfoods.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Olá, especialista - Tudo bem?";
+    }
+}
