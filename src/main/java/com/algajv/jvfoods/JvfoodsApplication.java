d34fafd781ca173af7723331c09e54a4699c4e23
xb216442 <p000joao.vitor.vieira@prservicos.com.br>
2020-01-24 11:38:06 -0300

    Project structure | Maven

---
+++ src/main/java/com/algajv/jvfoods/JvfoodsApplication.java
@@ -0,0 +1,13 @@
+package com.algajv.jvfoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvfoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvfoodsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algajv/jvfoods/JvfoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algajv.jvfoods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvfoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
